cef78d53a3deb7afc01d042251a991b0cc07a339
HAES1337 <password-remind@Mail.ru>
2022-12-13 14:19:23 +0300

    monday push

--- src/test/java/DepartmentTests/CreateDeleteDepartTest.java
+++ src/test/java/DepartmentTests/CreateDeleteDepartTest.java
@@ -1,10 +1,15 @@
 package DepartmentTests;
 import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
 
 import java.time.Duration;
 
@@ -13,7 +18,7 @@ public class CreateDeleteDepartTest extends AbstractBaseTest {
 
 
     @Test
-    public void CreateDeleteDepartTest() {
+    public void CreateDeleteDepartmentTest() throws InterruptedException {
         driver.navigate().to("http://develop.podruge.d1.3dev.tech/login/index.php");
         webDriverWait.until(ExpectedConditions.urlToBe("http://develop.podruge.d1.3dev.tech/login/index.php"));
         webDriverWait.until(ExpectedConditions.titleIs("Podruge Develop: Вход на сайт"));
@@ -45,12 +50,35 @@ public class CreateDeleteDepartTest extends AbstractBaseTest {
         webDriverWait.until(ExpectedConditions.titleIs("Подразделения"));
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 //        WebElement newDepart = driver.findElement(By.xpath("//button[@ type='button']"));
-        WebElement newDepart = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@ type='button']")));
+        WebElement newDepart = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@ class='header-group__buttons']")));
         newDepart.click();
-//        WebElement nameDepart = driver.findElement(By.id(":r7:"));
-        WebElement nameDepart = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@ type='text']")));
+        Thread.sleep(1000);
+        WebElement nameDepart = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='fitem_id_name']/div/div/div/input")));
+        nameDepart.isDisplayed();
         nameDepart.click();
-//        Название подразделения
-        nameDepart.sendKeys("Тестовое Название");
+        Thread.sleep(2000);
+//        JavascriptExecutor js1 = (JavascriptExecutor)driver;
+//        js1.executeScript("arguments[0].click();", nameDepart);
+//////        Название подразделения
+        nameDepart.sendKeys("НАЗВАНИЕ");
+        WebElement submit = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class,'css-8981b2')]/button[contains(text(),'Подтвердить')]")));
+        submit.click();
+        Thread.sleep(2000);
+        WebElement selectDropdown = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//form[@id='editForm']/div/div[4]/div[2]/div/div/div/div")));
+        selectDropdown.click();
+//        Select select = new Select(driver.findElement(By.xpath("//div[@role='presentation']")));
+//        select.selectByIndex(0);
+        Thread.sleep(2000);
+        new Actions(driver).keyDown(Keys.DOWN).sendKeys(Keys.ENTER).perform();
+        submit.click();
+        Thread.sleep(3000);
+        WebElement deleteDepart = driver.findElement(By.xpath("//div[@id='topofscroll']/div[2]/div/section/div/div/div/ul/li[last()]/div/div[4]/div[2]/a"));
+        deleteDepart.click();
+        WebElement confirmBtn = driver.findElement(By.xpath("//div[contains(@class,'css-8981b2')]/button"));
+        confirmBtn.click();
+//        WebElement select = driver.findElement(By.xpath("//div[@role='presentation'][3]/div/ul/li[1]"));
+//        select.click();
+
+        //form[@id='editForm']/div/div[4]/div[2]/div/div/div/div
     }
 }
